1d4315181664f6bee535dc2f80c1a18c2015d42f
John H <johnhayesru@gmail.com>
2019-02-03 03:54:11 +0300

    Completed setup for React with Babel/Webpack.

---
+++ app/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './src/containers/App'
+import './index.css'
+
+const app = document.getElementById('app')
+ReactDOM.render(<App />, app)
