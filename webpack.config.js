c519eb7cf180543400b92e5fe54d39874824bf98
John H <johnhayesru@gmail.com>
2019-02-06 23:02:27 +0300

    Changed background color variable, added build cleaning and optimization.

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
 import path from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 
+const VENDOR_LIBS = [ 'faker', 'tachyons', 'react', 'react-dom', 'react-input-range', 'react-redux', 'react-router', 'redux', 'redux-form', 'redux-thunk' ]
+
 module.exports = {
-  entry: './app/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
-  },
+  entry: { bundle: './app/index.js', vendor: VENDOR_LIBS },
+  output: { path: path.resolve(__dirname, 'build'), filename: '[name].[chunkhash].js' },
   module: {
     rules: [
-      { test: /\.(js|jsx)$/, use: 'babel-loader' },
+      { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ },
       { test: /\.(css|scss)$/, use: [ 'style-loader', 'css-loader', 'sass-loader' ] },
       { test: /\.(jpg|jpeg|png|gif|mp3|svg)$/, loaders: ['file-loader'] }
     ]
   },
   mode: 'development',
+  optimization: {
+    runtimeChunk: { name: 'manifest' },
+    splitChunks: { cacheGroups: { vendor: { chunks: 'initial', test: 'vendor', name: 'vendor', enforce: true } } }
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
