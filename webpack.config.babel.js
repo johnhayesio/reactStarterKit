777b5588c49c5edb18e3c0df5b3034348cc4942d
John H <johnhayesru@gmail.com>
2019-02-07 01:33:34 +0300

    Removed space from webpack.config.babel.js.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -23,7 +23,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'app/index.html'
     }),
-    new webpack.DefinePlugin ({
+    new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
   ]
