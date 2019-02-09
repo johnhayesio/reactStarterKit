5ad8fc3073fd6f83b97ce811fbb43f1f6dec5c82
John H <johnhayesru@gmail.com>
2019-02-09 04:43:44 +0300

    Fixed formatting in files.

--- app/src/containers/App.js
+++ app/src/containers/App.js
@@ -4,14 +4,20 @@ import logo from '../images/React.png'
 class App extends Component {
   render() {
     return (
-      <div className='vh-100 dt w-100'>
-        <div className='dtc v-mid tc'>
+      <div className="vh-100 dt w-100">
+        <div className="dtc v-mid tc">
           <div>
-            <img className='mw5' src={logo} alt='Logo'/>
+            <img className="mw5" src={logo} alt="Logo" />
           </div>
-          <div className='f2 f1-m f1-l sans-serif'>Welcome to React!</div>
-          <div className='f4 tracked-mega mb4'>2019 Starter Kit</div>
-          <a className='link br3 white gitlink' href='https://github.com/johnhayesio/reactStarterKit' target='_blank'>version 1.0.0</a>
+          <div className="f2 f1-m f1-l sans-serif">Welcome to React!</div>
+          <div className="f4 tracked-mega mb4">2019 Starter Kit</div>
+          <a
+            className="link br3 white gitlink"
+            href="https://github.com/johnhayesio/reactStarterKit"
+            target="_blank"
+          >
+            version 1.0.0
+          </a>
         </div>
       </div>
     )
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,15 +2,32 @@ import webpack from 'webpack'
 import path from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 
-const VENDOR_LIBS = [ 'faker', 'tachyons', 'react', 'react-dom', 'react-input-range', 'react-redux', 'react-router', 'redux', 'redux-form', 'redux-thunk' ]
+const VENDOR_LIBS = [
+  'faker',
+  'tachyons',
+  'react',
+  'react-dom',
+  'react-input-range',
+  'react-redux',
+  'react-router',
+  'redux',
+  'redux-form',
+  'redux-thunk'
+]
 
 module.exports = {
   entry: { bundle: './app/index.js', vendor: VENDOR_LIBS },
-  output: { path: path.resolve(__dirname, 'build'), filename: '[name].[chunkhash].js' },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].[chunkhash].js'
+  },
   module: {
     rules: [
       { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.(css|scss)$/, use: [ 'style-loader', 'css-loader', 'sass-loader' ] },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
       { test: /\.(jpg|jpeg|png|gif|mp3|svg)$/, loaders: ['file-loader'] }
     ]
   },
@@ -18,7 +35,16 @@ module.exports = {
   devServer: { port: 5555 },
   optimization: {
     runtimeChunk: { name: 'manifest' },
-    splitChunks: { cacheGroups: { vendor: { chunks: 'initial', test: 'vendor', name: 'vendor', enforce: true } } }
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: 'initial',
+          test: 'vendor',
+          name: 'vendor',
+          enforce: true
+        }
+      }
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
